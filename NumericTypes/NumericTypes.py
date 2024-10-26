53ca8cf92f0d1d1892f6d3e8c9ffa5d2a7ab3804
akhun <akhun@AZIZKHAN>
2024-10-26 13:26:32 +0500

    Добавьте файлы проекта.

---
+++ NumericTypes/NumericTypes.py
@@ -0,0 +1,7 @@
+                                    #Binary to Decimal
+def BinaryToDecimal(binaryNumber):
+    return int(binaryNumber, 2)
+
+
+binaryNumber = "1011"
+print(BinaryToDecimal(binaryNumber))
